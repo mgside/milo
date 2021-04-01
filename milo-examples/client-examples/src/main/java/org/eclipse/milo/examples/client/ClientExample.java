b2f39e025a2ee96528d9bf72dd7d53b94536d4ac
Gemicio <gemiciomustafa@gmail.com>
2021-04-01 13:01:44 +0300

    local and milo server tested milo has problems but local seems fine

--- milo-examples/client-examples/src/main/java/org/eclipse/milo/examples/client/ClientExample.java
+++ milo-examples/client-examples/src/main/java/org/eclipse/milo/examples/client/ClientExample.java
@@ -23,7 +23,9 @@ import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
 public interface ClientExample {
 
     default String getEndpointUrl() {
-        return "opc.tcp://localhost:12686/milo";
+    	
+    	return "opc.tcp://milo.digitalpetri.com:62541/milo";
+        //return "opc.tcp://localhost:12686/milo";
     }
 
     default Predicate<EndpointDescription> endpointFilter() {
